c5073db38e361ba5ee2332bed661b2b8c279cb26
nachikethprk <126313865+nachikethprk@users.noreply.github.com>
2023-03-10 14:19:44 +0530

    Add files via upload

---
+++ Std.jsx
@@ -0,0 +1,200 @@
+import React from 'react'
+
+const Std=(props)=> {
+  console.log(props);
+  return (
+    <>
+    <div className='main'>
+   
+        <div className='child'>
+            <img src={props.payload[0].Photo} alt="" />
+            <h2>{props.payload[0].Name}</h2>
+            <h2>{props.payload[0].Phoneno }</h2>
+            <h2>{props.payload[0].email}</h2>
+            <h2>{props.payload[0].age}</h2>
+            <h2>{props.payload[0].gender}</h2>
+        </div>
+        <div className='child'>
+       <img src={props.payload[1].Photo} alt="" />
+            <h2>{props.payload[1].Name}</h2>
+            <h2>{props.payload[1].Phoneno }</h2>
+            <h2>{props.payload[1].email}</h2>
+            <h2>{props.payload[1].age}</h2>
+            <h2>{props.payload[1].gender}</h2>
+        </div>
+
+        <div className='child'>
+       <img src={props.payload[2].Photo} alt="" />
+            <h2>{props.payload[2].Name}</h2>
+            <h2>{props.payload[2].Phoneno }</h2>
+            <h2>{props.payload[2].email}</h2>
+            <h2>{props.payload[2].age}</h2>
+            <h2>{props.payload[2].gender}</h2>
+        </div>
+
+        <div className='child'>
+       <img src={props.payload[3].Photo} alt="" />
+            <h2>{props.payload[3].Name}</h2>
+            <h2>{props.payload[3].Phoneno }</h2>
+            <h2>{props.payload[3].email}</h2>
+            <h2>{props.payload[3].age}</h2>
+            <h2>{props.payload[3].gender}</h2>
+        </div>
+
+        <div className='child'>
+       <img src={props.payload[4].Photo} alt="" />
+            <h2>{props.payload[4].Name}</h2>
+            <h2>{props.payload[4].Phoneno }</h2>
+            <h2>{props.payload[4].email}</h2>
+            <h2>{props.payload[4].age}</h2>
+            <h2>{props.payload[4].gender}</h2>
+        </div>
+      
+        </div>
+
+        <div className='main'>
+        <div className='child'>
+       <img src={props.payload[5].Photo} alt="" />
+            <h2>{props.payload[5].Name}</h2>
+            <h2>{props.payload[5].Phoneno }</h2>
+            <h2>{props.payload[5].email}</h2>
+            <h2>{props.payload[5].age}</h2>
+            <h2>{props.payload[5].gender}</h2>
+        </div>
+
+        <div className='child'>
+       <img src={props.payload[6].Photo} alt="" />
+            <h2>{props.payload[6].Name}</h2>
+            <h2>{props.payload[6].Phoneno }</h2>
+            <h2>{props.payload[6].email}</h2>
+            <h2>{props.payload[6].age}</h2>
+            <h2>{props.payload[6].gender}</h2>
+        </div>
+
+        <div  className='child'>
+       <img src={props.payload[7].Photo} alt="" />
+            <h2>{props.payload[7].Name}</h2>
+            <h2>{props.payload[7].Phoneno }</h2>
+            <h2>{props.payload[7].email}</h2>
+            <h2>{props.payload[7].age}</h2>
+            <h2>{props.payload[7].gender}</h2>
+        </div>
+
+        <div className='child'>
+       <img src={props.payload[8].Photo} alt="" />
+            <h2>{props.payload[8].Name}</h2>
+            <h2>{props.payload[8].Phoneno }</h2>
+            <h2>{props.payload[8].email}</h2>
+            <h2>{props.payload[8].age}</h2>
+            <h2>{props.payload[8].gender}</h2>
+        </div>
+
+        <div className='child'>
+       <img src={props.payload[9].Photo} alt="" />
+            <h2>{props.payload[9].Name}</h2>
+            <h2>{props.payload[9].Phoneno }</h2>
+            <h2>{props.payload[9].email}</h2>
+            <h2>{props.payload[9].age}</h2>
+            <h2>{props.payload[9].gender}</h2>
+        </div>
+</div>
+
+<div className='main'>
+        <div className='child'>
+       <img src={props.payload[10].Photo} alt="" />
+            <h2>{props.payload[10].Name}</h2>
+            <h2>{props.payload[10].Phoneno }</h2>
+            <h2>{props.payload[10].email}</h2>
+            <h2>{props.payload[10].age}</h2>
+            <h2>{props.payload[10].gender}</h2>
+        </div>
+
+        <div className='child'> 
+       <img src={props.payload[11].Photo} alt="" />
+            <h2>{props.payload[11].Name}</h2>
+            <h2>{props.payload[11].Phoneno }</h2>
+            <h2>{props.payload[11].email}</h2>
+            <h2>{props.payload[11].age}</h2>
+            <h2>{props.payload[11].gender}</h2>
+        </div>
+
+        <div className='child'>
+       <img src={props.payload[12].Photo} alt="" />
+            <h2>{props.payload[12].Name}</h2>
+            <h2>{props.payload[12].Phoneno }</h2>
+            <h2>{props.payload[12].email}</h2>
+            <h2>{props.payload[12].age}</h2>
+            <h2>{props.payload[12].gender}</h2>
+        </div>
+
+        <div className='child'>
+       <img src={props.payload[13].Photo} alt="" />
+            <h2>{props.payload[13].Name}</h2>
+            <h2>{props.payload[13].Phoneno }</h2>
+            <h2>{props.payload[13].email}</h2>
+            <h2>{props.payload[13].age}</h2>
+            <h2>{props.payload[13].gender}</h2>
+        </div>
+
+        <div className='child'>
+       <img src={props.payload[14].Photo} alt="" />
+            <h2>{props.payload[14].Name}</h2>
+            <h2>{props.payload[14].Phoneno }</h2>
+            <h2>{props.payload[14].email}</h2>
+            <h2>{props.payload[14].age}</h2>
+            <h2>{props.payload[14].gender}</h2>
+        </div>
+          </div>
+
+<div className='main'>
+        <div className='child'>
+       <img src={props.payload[15].Photo} alt="" />
+            <h2>{props.payload[15].Name}</h2>
+            <h2>{props.payload[15].Phoneno }</h2>
+            <h2>{props.payload[15].email}</h2>
+            <h2>{props.payload[15].age}</h2>
+            <h2>{props.payload[15].gender}</h2>
+        </div>
+
+        <div className='child'>
+       <img src={props.payload[16].Photo} alt="" />
+            <h2>{props.payload[16].Name}</h2>
+            <h2>{props.payload[16].Phoneno }</h2>
+            <h2>{props.payload[16].email}</h2>
+            <h2>{props.payload[16].age}</h2>
+            <h2>{props.payload[16].gender}</h2>
+        </div>
+
+        <div className='child'>
+       <img src={props.payload[17].Photo} alt="" />
+            <h2>{props.payload[17].Name}</h2>
+            <h2>{props.payload[17].Phoneno }</h2>
+            <h2>{props.payload[17].email}</h2>
+            <h2>{props.payload[17].age}</h2>
+            <h2>{props.payload[17].gender}</h2>
+        </div>
+
+        <div className='child'>
+       <img src={props.payload[18].Photo} alt="" />
+            <h2>{props.payload[18].Name}</h2>
+            <h2>{props.payload[18].Phoneno }</h2>
+            <h2>{props.payload[18].email}</h2>
+            <h2>{props.payload[18].age}</h2>
+            <h2>{props.payload[18].gender}</h2>
+        </div>
+
+        <div className='child'>
+       <img src={props.payload[19].Photo} alt="" />
+            <h2>{props.payload[19].Name}</h2>
+            <h2>{props.payload[19].Phoneno }</h2>
+            <h2>{props.payload[19].email}</h2>
+            <h2>{props.payload[19].age}</h2>
+            <h2>{props.payload[19].gender}</h2>
+        </div>
+
+       </div>
+</>
+    
+  )
+}
+export default Std
